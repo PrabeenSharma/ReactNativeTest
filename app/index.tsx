9a12124a0a2a948f7fa8f31aeec06ada11647809
Indranil <indranil@virginworkz.com>
2026-03-27 19:00:10 +0530

    App

--- app/index.tsx
+++ app/index.tsx
@@ -12,10 +12,12 @@ export default function HomeScreen() {
     <View style={{ flex: 1 , alignItems: 'center', padding: 30, }}>
     <Text style={{ paddingBottom: 30, fontSize: 24, fontWeight:800  }}>Welcome to Red Planel Resort </Text>
 
-    <View style={{ flex:1, flexDirection:'row', gap:20 }}>
+    <View style={{ flex:1, flexDirection:'row', gap:20, }}>
       <Link style={styles.linkStyle} href="/scanner">scanner</Link>
       <TouchableOpacity style={styles.linkStyle} onPress={() => Linking.openURL('https://dev4work.com/thefirstonmars/')}>Buy Ticket</TouchableOpacity>
     </View>
+
+    <Link style={styles.linkStyleRed} href="/dashboard">Dashboard</Link>
 
 
     </View>
@@ -28,6 +30,9 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   linkStyle:{
     backgroundColor: '#00ddf1', paddingLeft:15, paddingRight:15, paddingTop:12, paddingBottom:12, 
+  },
+  linkStyleRed:{
+    backgroundColor: 'red', paddingLeft:15, paddingRight:15, paddingTop:12, paddingBottom:12, marginTop:20, color:'#fff'
   },
   titleContainer: {
     flexDirection: 'row',
